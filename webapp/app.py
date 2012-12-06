088e1458e1d68ca14309d1d3348bcb1423bf52eb
Geniass Mchlachla <geniassmchlachla@gmail.com>
2012-12-07 00:32:02 +0200

    logs again

--- webapp/app.py
+++ webapp/app.py
@@ -1,23 +1,46 @@
 from flask import Flask, render_template, redirect, url_for, g, request
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    app.logger.debug("HOMEPAGE!")
+    print >> sys.stderr, "HOMEPAGE!"
     return render_template('homepage.html')
 
 
 @app.route('/users.xml', methods=['POST'])
 def users():
     if request.method == 'POST':
-        app.logger.debug(request.data)
+        #app.logger.debug(request.data)
+        print >> sys.stderr, request.data
+        #print_stderr(request.data)
 
 
 @app.route('/devices.xml', methods=['POST'])
 def devices():
     if request.method == 'POST':
-        app.logger.debug(request.data)
+        #app.logger.debug(request.data)
+        print_stderr(request.data)
+
+
+@app.route('profile.xml', methods=['POST'])
+def profile():
+    if request.method == 'POST':
+        #app.logger.debug(request.data)
+        print_stderr(request.data)
+
+
+@app.route('reports.xml', methods=['POST'])
+def reports():
+    if request.method == 'POST':
+        #app.logger.debug(request.data)
+        print_stderr(request.data)
+
+
+
+def print_stderr(message):
+    print >> sys.stderr, message
 
 
 if __name__ == '__main__':

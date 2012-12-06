8a0bcf77b6a712b4c3d191c10064484280b46e09
Geniass Mchlachla <geniassmchlachla@gmail.com>
2012-12-06 23:55:33 +0200

    log

--- webapp/app.py
+++ webapp/app.py
@@ -4,11 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    app.logger.debug("HOMEPAGE!")
     return render_template('homepage.html')
 
 
 @app.route('/users.xml', methods=['POST'])
 def users():
+    if request.method == 'POST':
+        app.logger.debug(request.data)
+
+
+@app.route('/devices.xml', methods=['POST'])
+def devices():
     if request.method == 'POST':
         app.logger.debug(request.data)
 

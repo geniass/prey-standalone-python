60ce9d574b3a8b637681f86b51cf2cd682cc4f63
Geniass Mchlachla <geniassmchlachla@gmail.com>
2012-12-18 13:55:53 +0200

    moar testing

--- webapp/app.py
+++ webapp/app.py
@@ -111,7 +111,7 @@ def logout():
 def users():
     if request.method == 'POST':
         print_stderr("Users.xml Request.data: " + str(request.data))
-        print_stderr("Users.xml Other thing: " + str(request.form.keys()[0]))
+        print_stderr("Users.xml Other thing: " + str(request.form.keys()))
 
         keyvalue = prey_params_dict(request.data)
         print_stderr("POST Data (users.xml): " + str(keyvalue))

9fdf0cd9023676e25440f02380ad3b4b54408126
Geniass Mchlachla <geniassmchlachla@gmail.com>
2012-12-18 19:28:05 +0200

    sdfsdj debugging

--- webapp/app.py
+++ webapp/app.py
@@ -314,8 +314,9 @@ def reports_xml(device_id):
         user = users_collection.find_one(g.user._id)
         if device['api_key'] == user['api_key']:
             reports = reports_collection.find({'device_id': device_id})
+            print_stderr(reports.count)
             if reports.count == 0:
-                flash("There ar eno reports for this device", "warning")
+                flash("There are no reports for this device", "warning")
             return render_template('reports_basic.html', reports=reports)
         else:
             flash("Editing URL's? That's not your device!", "warning")
